bd7fbe40249b37d7d1e80b2eb83875eb3afe768f
Zee Alexander <zee@zee.space>
2016-05-20 11:57:13 -0700

    Bugfix version bump for enabling facter cops

--- lib/rubocop/facter/version.rb
+++ lib/rubocop/facter/version.rb
@@ -3,7 +3,7 @@ module RuboCop
     # Central reference point for current version of rubocop-facter
     module Version
       # rubocop:disable all
-      VERSION = '0.2.0'
+      VERSION = '0.2.1'
       # rubocop:enable all
 
       def self.current
